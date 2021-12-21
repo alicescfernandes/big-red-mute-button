5e09e39dca4ecda4797f65e95b835d98765f76cc
Alice Fernandes <alicescfernandes@gmail.com>
2021-12-21 13:15:34 +0000

    arduino sketch

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,12 @@
+#define PIN_LED 9
+#define PIN_BTN 2 // must have interrupts on pin
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3,OUTPUT);
-  digitalWrite(3,HIGH); //RELAY
-  attachInterrupt(digitalPinToInterrupt(2), teste, FALLING);
+  pinMode(PIN_BTN, INPUT);
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED,LOW);    
+  attachInterrupt(digitalPinToInterrupt(PIN_BTN), teste, FALLING);
 }
 
 unsigned long int lastTime = 0;
@@ -17,17 +19,19 @@ void teste() {
     
 }
 
-char buf[1];
+
 void loop() {
+
   if(Serial.available()){
-    byte statusByte = Serial.readBytes(buf,1);
-    if(buf[0] == '1'){
-      digitalWrite(3,LOW);
+    
+    byte byteChar = Serial.read();
+    if(byteChar == 48){
+      digitalWrite(PIN_LED,LOW);
      
     }
 
-    if(buf[0] == '0'){
-      digitalWrite(3,HIGH); //RELAY
+    if(byteChar == 49){
+      digitalWrite(PIN_LED,HIGH);
      }
   }
 }
