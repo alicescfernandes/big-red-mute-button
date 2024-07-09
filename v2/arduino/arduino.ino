cea39456e150108aa01065450b44a31bf4ee58a9
Alice Fernandes <alicescfernandes@gmail.com>
2024-07-09 17:29:16 +0100

    consistent code

--- v2/arduino/arduino.ino
+++ v2/arduino/arduino.ino
@@ -5,8 +5,8 @@
 #define RED_LED D2
 #define RED_BTN D10
 
-NimBLEServer *bleServer;
-NimBLECharacteristic *btnCharacteristic;
+NimBLEServer *ble_server;
+NimBLECharacteristic *btn_characteristic;
 
 bool deviceConnected = false;
 bool prev_reading;
@@ -24,13 +24,13 @@ class ServerCallbacks : public NimBLEServerCallbacks {
 
   void onDisconnect(NimBLEServer *pServer) {
     deviceConnected = false;
-    Serial.println("connected");
+    Serial.println("disconnected");
   }
 };
 
-int readValue(NimBLECharacteristic *c) {
+int read_value(NimBLECharacteristic *c) {
   time_t timestamp;
-  NimBLEAttValue stuff = btnCharacteristic->getValue();  // timestamp optional
+  NimBLEAttValue stuff = btn_characteristic->getValue();  // timestamp optional
   const uint8_t *value = stuff.getValue(&timestamp);
   return (int)value[0];
 }
@@ -38,7 +38,7 @@ int readValue(NimBLECharacteristic *c) {
 class BoxWriteCallbacks : public NimBLECharacteristicCallbacks {
   void onWrite(NimBLECharacteristic *pCharacteristic) {
     Serial.println("someone wrote here");
-    int value = readValue(pCharacteristic);
+    int value = read_value(pCharacteristic);
 
     button_state = value ? true : false;
     digitalWrite(RED_LED, button_state ? HIGH : LOW);
@@ -48,7 +48,7 @@ class BoxWriteCallbacks : public NimBLECharacteristicCallbacks {
 // https://www.bluetooth.com/specifications/dis-1-2/
 void create_dis_service() {
   // Create device info service (DIS)
-  NimBLEService *service = bleServer->createService("180A");
+  NimBLEService *service = ble_server->createService("180A");
 
   // Create Manufacturer Name String characteristic, read only
   NimBLECharacteristic *characteristic = service->createCharacteristic("2A29", NIMBLE_PROPERTY::READ);
@@ -59,7 +59,7 @@ void create_dis_service() {
 
 // https://www.bluetooth.com/specifications/specs/battery-service/
 void create_bas_service() {
-  NimBLEService *service = bleServer->createService("180F");
+  NimBLEService *service = ble_server->createService("180F");
 
   NimBLECharacteristic *characteristic = service->createCharacteristic("2A19", NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY);
   characteristic->setValue(100);  // Clients can subscribe to this, and update as they wish
@@ -68,12 +68,12 @@ void create_bas_service() {
 }
 
 void create_box_service() {
-  NimBLEService *service = bleServer->createService(BUTTON_SERVICE_UUID);
-  // btnCharacteristic = service->createCharacteristic(BTN_CHAR, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::READ_ENC | NIMBLE_PROPERTY::WRITE_ENC | NIMBLE_PROPERTY::READ_AUTHEN | NIMBLE_PROPERTY::WRITE_AUTHEN | NIMBLE_PROPERTY::NOTIFY);
+  NimBLEService *service = ble_server->createService(BUTTON_SERVICE_UUID);
+  // btn_characteristic = service->createCharacteristic(BTN_CHAR, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::READ_ENC | NIMBLE_PROPERTY::WRITE_ENC | NIMBLE_PROPERTY::READ_AUTHEN | NIMBLE_PROPERTY::WRITE_AUTHEN | NIMBLE_PROPERTY::NOTIFY);
 
-  btnCharacteristic = service->createCharacteristic(BTN_CHAR, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
-  btnCharacteristic->setValue(byte(0));  // Clients can subscribe to this, and update as they wish
-  btnCharacteristic->setCallbacks(new BoxWriteCallbacks());
+  btn_characteristic = service->createCharacteristic(BTN_CHAR, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
+  btn_characteristic->setValue(byte(0));  // Clients can subscribe to this, and update as they wish
+  btn_characteristic->setCallbacks(new BoxWriteCallbacks());
   service->start();
 }
 
@@ -90,8 +90,8 @@ void setup() {
   NimBLEDevice::setSecurityPasskey(PASSKEY);
   NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY);
 
-  bleServer = NimBLEDevice::createServer();
-  bleServer->setCallbacks(new ServerCallbacks());
+  ble_server = NimBLEDevice::createServer();
+  ble_server->setCallbacks(new ServerCallbacks());
 
   create_dis_service();
   create_bas_service();
@@ -120,6 +120,6 @@ void loop() {
     Serial.println(button_state);
     Serial.println(button_state ? HIGH : LOW);
     digitalWrite(RED_LED, button_state ? HIGH : LOW);
-    write_value(btnCharacteristic, button_state ? 1 : 0);
+    write_value(btn_characteristic, button_state ? 1 : 0);
   }
 }
