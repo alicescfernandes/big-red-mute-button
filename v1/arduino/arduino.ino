195ed722905150bee23a311d4de842fff2b0bb8b
Alice Fernandes <alicescfernandes@gmail.com>
2024-07-09 11:18:21 +0100

    move to folder

---
+++ v1/arduino/arduino.ino
@@ -0,0 +1,37 @@
+#define PIN_LED 9
+#define PIN_BTN 2 // must have interrupts on pin
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(PIN_BTN, INPUT);
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED,LOW);    
+  attachInterrupt(digitalPinToInterrupt(PIN_BTN), teste, FALLING);
+}
+
+unsigned long int lastTime = 0;
+void teste() {
+  unsigned long int currentTime = millis();
+    if((currentTime - lastTime) > 200){
+       Serial.println('0');
+       lastTime = millis();
+    }
+    
+}
+
+
+void loop() {
+
+  if(Serial.available()){
+    
+    byte byteChar = Serial.read();
+    if(byteChar == 48){
+      digitalWrite(PIN_LED,LOW);
+     
+    }
+
+    if(byteChar == 49){
+      digitalWrite(PIN_LED,HIGH);
+     }
+  }
+}
